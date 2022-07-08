6fe1b1df28d508ffc5b83dbb2114bf2428808d89
Ukoette <uko4christ22@gmail.com>
2022-07-07 23:51:36 -0700

    My c debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,9 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 
-/**
- * main - tests function that prints if integer is positive or negative
- * Return: zero
- */
-
-int main(void)
-{
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (o);
-}
+void positive_or_negative(int i);
+int larget_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif /* MAIN_H */
