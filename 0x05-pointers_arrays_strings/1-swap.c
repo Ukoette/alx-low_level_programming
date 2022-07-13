2ec9ec53175cbbd07ba5a976eaa85578ac275374
Ukoette <uko4christ22@gmail.com>
2022-07-12 18:27:47 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+
+/**
+ * swap_int - swaps value of int a and int b
+ * @a: first int to swap
+ * @b: second int to swap
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
