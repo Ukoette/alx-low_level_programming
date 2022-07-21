91ce00afc332b8ef5e4316d261bbfa5196073ea1
Ukoette <uko4christ22@gmail.com>
2022-07-21 15:30:14 -0700

    uk

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,5 +6,29 @@
  * @c: iterator.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
-int is \_prime(unsigned int n, unsigned int c)
-
+int is_prime(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (0);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
+}
+/**
+ * is_prime_number - detects if an input number is a prime number.
+ * @n: input number.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ */
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
+}
