dd6de6be4ce08d70fa81a2e5373a6bc8be1dcd5d
Ukoette <uko4christ22@gmail.com>
2022-08-19 21:09:34 -0700

    my commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,7 +8,7 @@
  *
  * Return: 1 if it worked, -1 if it didd't.
  */
-int set_bit(unsigned long int*n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int m;
 
