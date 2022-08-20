12b762a702e2f4a9aee4bae5ff74c0a77e5b6747
Ukoette <uko4christ22@gmail.com>
2022-08-19 21:46:38 -0700

    cmit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,12 +7,12 @@
  *
  * Return: no return.
  */
-void print_binary(unsigned longint n)
+void print_binary(unsigned long int n)
 {
 	if (n >> 1)
 	{
-		prin_binary(n >> 1);
-	_putchar(n & 1) + '0');
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 	}
 	else
 	{
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,7 @@
 #define __MAIN_H__
 
 
-
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
