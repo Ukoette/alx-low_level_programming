ce96d973385e0ac291ce11cd8e97269d46fcebd2
Ukoette <uko4christ22@gmail.com>
2022-08-23 21:58:19 -0700

    my i/o project

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+#include <stdlib.h>
+
+
+/**
+ * read_textfile - Read a text file and print it to POSIX stdout.
+ * @filename: Apointer to the name of the file.
+ * @letter: The number of letters the 
+ * function should return.
+ *
+ * Return: If the function fails or filename id NULL -0.
+ *
+ * */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+
+	close(o);
+
+
+	return (w);
